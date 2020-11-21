31a5214ab0b93090b6e021c3f7037c8fc40427c1
mountainray <mountainray@hotmail.com>
2020-11-21 15:35:17 -0700

    final version, removed commented code, final adjustments

--- server.R
+++ server.R
@@ -1,6 +1,4 @@
 shinyServer(function(input, output) {
-# dfx<-data.frame(read_csv("https://opendata.arcgis.com/datasets/1456d8d43486449292e5784dcd9ce4a7_0.csv"),
-# 		stringsAsFactors = F)
 
 	output$plotmapx<-renderPlot({
 	jj<-filter(dfx2,
@@ -19,28 +17,14 @@ ggplot(data = world) +
     coord_sf(xlim = c(-109.2, -102), ylim = c(36.9, 41.2), expand = FALSE)
 })
 
-# output$plotx<-renderPlot({
-# 	jj<-filter(dfx2,
-# 		   Desc_ %in% input$measure_selector &
-# 		   	County %in% input$county_selector &
-# 		   	newdate>=as.Date("2020-01-01"))
-#
-# 	ggplot(data=jj, mapping=aes(x=newdate, y=newvalue, color=County), size=2)+
-# 	       	geom_line()+
-# 		geom_smooth(data=all, mapping = aes(x=datex, y=hospitality_2020), color="orange")+
-# 		ggtitle(input$measure_selector)
-# })
-
 output$plotx<-renderPlot({
 hospitality_2020<-c(4.8,6.0,7.2,7.4,8.0)*100
 dates<-c("2020-04-20","2020-05-20","2020-06-20",
 	 "2020-07-20","2020-08-20")
-####
+
 xxx<-function(weeks){
 	(all<-data.frame(
-	#construction_2020,
 	hospitality_2020,
-	#trade_2020,
 	datex=as.Date(dates))%>%filter(datex>="2020-04-01"))
 
 allnew<-all
@@ -57,17 +41,6 @@ fit<-glm(newvalue~hospitality_2020, dfx4, family = "poisson")
 
 round(sum((dfx4$newvalue-fit$fitted.values)^2))
 }
-# x0<-xxx(0)
-# x1<-xxx(1)
-# x2<-xxx(2)
-# x3<-xxx(3)
-# x4<-xxx(4)
-# x5<-xxx(5)
-# x6<-xxx(6)
-# x7<-xxx(7)
-# x8<-xxx(8)
-# x9<-xxx(9)
-# x10<-xxx(10)
 x0<-xxx(0)
 x1<-xxx(7)
 x2<-xxx(14)
@@ -79,16 +52,17 @@ x7<-xxx(49)
 x8<-xxx(56)
 x9<-xxx(63)
 x10<-xxx(70)
-#x<-data.frame(delay=c(x0,x1,x2,x3,x4,x5,x6,x7))
-#x<-data.frame(delay=c(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10), weeks=c(0:10))
+
 x<-data.frame(delay=c(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9,x10), weeks=c(str_c(" ",as.character(c(0:9))),"10"), stringsAsFactors = F)
+
 labelsx<-as.character(c(0:10))
+
 theselection<-x[which(as.numeric(x$weeks)==input$delay_selector),]
+
 theminimum<-x[which(as.numeric(x$delay)==min(x$delay)),]
-#thecolor<-ifelse(theselection==theminimum, "green", "yellow")
+
 thecolor<<-ifelse(theselection$delay==theminimum$delay, "green", "yellow")
-###
-#	ggplot(data=x, mapping=aes(x=x))+
+
 	ggplot(data=x, mapping=aes(x=weeks,y=delay))+
 	       	geom_bar(stat = "identity", fill="blue")+
 	       	geom_bar(data=theminimum, mapping=aes(x=weeks, y=delay),stat = "identity", fill="green")+
@@ -96,12 +70,9 @@ thecolor<<-ifelse(theselection$delay==theminimum$delay, "green", "yellow")
 	       	geom_bar(data=theselection, mapping=aes(x=weeks, y=delay),stat = "identity", fill=thecolor)+
 		xlab("Weeks Delay")+ylab("Total SE")+
 		theme_dark() + theme(panel.background=element_rect("black")) +theme(panel.grid=element_line("black"))+
-	# 	theme(axis.ticks=element_line(F))+theme(axis.text=element_text(""))+theme(axis.title=element_text(""))
-#		ggtitle(input$measure_selector)
-		ggtitle("Total SE")
+		ggtitle("Sum of Squared Errors, Cases ~ Hospitality")
 })
 
-#reactive(captionx<<-str_c("Raw county data (",input$measure_selector,")"))
 output$tablex<-renderTable({
 	head(filter(dfx2,
 		   Desc_==input$measure_selector &
@@ -110,25 +81,17 @@ output$tablex<-renderTable({
 		select(County, Date, newvalue)%>%arrange(desc(Date), County)%>%
 			pivot_wider(names_from = County, values_from = newvalue)
 		,10)
-	# captionx<-str_c("Raw county data (",input$measure_selector,")")
 	},caption="Raw county data, previous 10 days")
-	#},caption=captionx)
-	#},caption=str_c("Raw county data (",input$measure_selector,")"))
 
 output$plotx2<-renderPlot({
 
-# hospitality_2020<-c(7.5,7.6,7.9,4.8,6.0,7.2,7.4,8.0,7.9,7.9)*100
-# dates<-c("2020-01-20","2020-02-20","2020-03-20","2020-04-20","2020-05-20","2020-06-20",
-# 	 "2020-07-20","2020-08-20","2020-09-20", "2020-10-20")
 hospitality_2020<-c(4.8,6.0,7.2,7.4,8.0)*100
 dates<-c("2020-04-20","2020-05-20","2020-06-20",
 	 "2020-07-20","2020-08-20")
 
-(all<-data.frame(
-	#construction_2020,
+all<-data.frame(
 	hospitality_2020,
-	#trade_2020,
-	datex=as.Date(dates))%>%filter(datex>="2020-04-01"))
+	datex=as.Date(dates))%>%filter(datex>="2020-04-01")
 
 allnew<-all
 allnew$datex<-allnew$datex+input$delay_selector*7
@@ -137,7 +100,7 @@ dfx3<-inner_join(dfx2, allnew, by = c("newdate"="datex"))%>%
 	filter(Desc_==input$measure_selector & County %in% c(input$county_selector))%>%
 	group_by(newdate)%>%
 	summarise(newvalue=round(mean(newvalue)))%>%ungroup()
-(dfx4<-data.frame(dfx3, hospitality_2020=allnew[1:nrow(dfx3),1]))
+dfx4<-data.frame(dfx3, hospitality_2020=allnew[1:nrow(dfx3),1])
 allnew<-allnew[1:nrow(dfx3),]-1+(input$delay_selector+1)/(input$delay_selector+1)
 
 fit<-glm(newvalue~hospitality_2020, dfx4, family = "poisson")
@@ -150,16 +113,12 @@ box<-str_c("Total SE\n",round(sum((dfx4$newvalue-fit$fitted.values)^2)))
 
 ggplot()+
 	geom_smooth(mapping = aes(x=allnew$datex, y=fit$fitted.values), color=thecolor, se=F, size=4)+
-#	geom_point(mapping = aes(x=allnew$datex, y=fit$fitted.values), color="blue", pch=19, size=4)+
 	geom_smooth(data=filter(dfx2, Desc_==input$measure_selector & County %in% c(input$county_selector)),
 		    mapping = aes(x=newdate, y=newvalue, color=County), se=F, alpha=.3)+
 	geom_point(data=filter(dfx2, Desc_==input$measure_selector & County %in% c(input$county_selector)),
 		   mapping = aes(x=newdate, y=newvalue, color=County), pch=21, size=4, alpha=.3, stroke=2)+
-	# geom_smooth(mapping = aes(x=allnew$datex, y=fit$fitted.values), color="yellow", se=F, size=4, alpha=.4)+
-	# geom_point(mapping = aes(x=allnew$datex, y=fit$fitted.values), color="blue", pch=21, size=4)+
 	xlab(label = "Date")+ylab(label = input$measure_selector)+
 		theme_dark() + theme(panel.background=element_rect("black")) +theme(panel.grid=element_line("black"))+
-#	geom_label(mapping = aes(x=as.Date("2020-04-15"), y=1500), label=box, fill="yellow")
 	geom_label(mapping = aes(x=as.Date("2020-04-15"), y=1500), label=box, fill=thecolor, size=7)+
 	ggtitle(input$measure_selector)
 })
--- ui.R
+++ ui.R
@@ -18,39 +18,22 @@ dfx<<-data.frame(read_csv("https://opendata.arcgis.com/datasets/1456d8d434864492
 	       fips=str_c("08",FIPS), newcolor="blue")%>% # combine Rate and Value columns
 	arrange(newdate, fips, Desc_)%>%rename(County=LABEL)
 dfx2<<-dfx%>%
-#	filter(LABEL %in% c("Montezuma", "La Plata", "Dolores", "San Juan", "San Miguel",
-#			    "Mineral", "Hinsdale", "Montrose", "Archuleta"))%>% # SW CO Counties
 	select(-COUNTY, -FULL_, -Value, -Rate, -ObjectId)
 head(dfx2)
-
-# hospitality_2020<-c(7.5,7.6,7.9,4.8,6.0,7.2,7.4,8.0,7.9,7.9)*100
-# dates<-c("2020-01-20","2020-02-20","2020-03-20","2020-04-20","2020-05-20","2020-06-20",
-# 	 "2020-07-20","2020-08-20","2020-09-20", "2020-10-20")
-#
-# (all<-data.frame(
-# 	#construction_2020,
-# 	hospitality_2020,
-# 	#trade_2020,
-# 	datex=as.Date(dates))%>%filter(datex>="2020-04-01"))
 
 world <<- ne_countries(scale = "medium", returnclass = "sf", continent = "north america", country = "united states of america")
 
 counties <<- st_as_sf(map("county", plot = FALSE, fill = TRUE))
 counties <<- subset(counties, grepl("colorado", counties$ID))
 counties$area <<- as.numeric(st_area(counties))
-(county_names<<-st_as_sf(st_centroid(counties))$data)
-(county_names<<-st_centroid(counties))
-str(st_centroid(counties))
-length(county_names$ID)
-county_names$geom[1]
-(county_names$geom[1])
-
+county_names<<-st_as_sf(st_centroid(counties))$data
+county_names<<-st_centroid(counties)
 
 shinyUI(navbarPage("COVID-19 Delay Application", theme = "bootstrap.css",
-#shinyUI(fluidPage(
-#    titlePanel(HTML("Colorado County-level COVID data<br></br><h4>Detecting delayed effects of changes in Hospitality Employment levels</h4>")),
 tabPanel("Documentation", fluidPage(HTML("
+
 <h2>Documentation...</h3>
+
 <h3>Concept</h3>
 <p>With the pandemic raging, it was of interest to see how well one could assess when the ramifications of <q>opening up</q> would
 be observed as increased case counts.  Obviously monthly employment level data (in this case for Hospitality) over 2020 has seen some
@@ -64,6 +47,7 @@ given public health administration guidelines.</p>
 <a href='https://opendata.arcgis.com/datasets/1456d8d43486449292e5784dcd9ce4a7_0.csv'>here</a>.</p>
 <p>The Hospitality employment level data was obtained from the Bureau of Labor Statistics, local and county data.  As my location is in
 southwest Colorado, the <a href='https://www.bls.gov/regions/mountain-plains/co_grandjunction_msa.htm#eag'>Grand Junction</a> data were more applicable.</p>
+
 <h3>Processing</h3>
 <p>The main plot indicates the Cases per County, across dates.  In the plot, the yellow line is a plot of the
 Hospitality employment.  In this case, only the months capturing the aggressive increases in <q>opening up</q>.</p>
@@ -76,9 +60,9 @@ model residuals (SSE), with the lowest/best fit signaled in green.</p>
 <p>As you investigate/increase the delay, the model has a better fit in the future, and at some point drops off in accuracy.
  Therefore this application can give one a feel for the length of time it takes for an observed/new increase in employment to
 manifest as an increase in Cases.</p>"))),
+
 tabPanel("Main",
     titlePanel(HTML("Colorado County-level COVID data\n<h4>Detecting delayed effects of changes in Hospitality Employment levels</h4><br></br>")),
-    # fluidRow("a try"),
     sidebarLayout(
         sidebarPanel(
     fluidRow(h3("Instructions..."),HTML("Pick at least 1 <b>County</b>, the map below will indicate its location.  Choose a <b>measure</b>, then <b>delay in weeks</b>.<br></br>")),
@@ -93,49 +77,19 @@ tabPanel("Main",
                         choiceNames = as.list(unique(dfx2$Desc_)), choiceValues = as.list(unique(dfx2$Desc_))),
             sliderInput("delay_selector",
                         "Choose delay...",value = 0,
-            	    min = 0, max = 10, animate = T, post = " Weeks"
-),
+            	    min = 0, max = 10, animate = T, post = " Weeks"),
 helpText("The slider above determines the delay (in weeks) from observed changes in Hospitality industry employment levels.
-	 Press the Play button to cycle through them all, and observe the fit of the model lining up (or not) with Cases.")
-
-        ),
+	 Press the Play button to cycle through them all, and observe the fit of the model lining up (or not) with Cases.")),
 
         mainPanel(
             plotOutput("plotx2", width = "800px", height = "600px"),
             plotOutput("plotx", width = "400px", height = "200px"),
-	    helpText("Sum of Squared Errors, Cases ~ Hospitality"),
-            tableOutput("tablex")
-        )
-    )
-),
-# tabPanel("Documentation", fluidPage(HTML("
-# <h2>Documentation...</h3><br></br>
-# <h3>Data</h3><br></br>
-# <p>The Colorado Case data on COVID-19 positivity is updated daily, and can be found here:<br></br>
-# https://opendata.arcgis.com/datasets/1456d8d43486449292e5784dcd9ce4a7_0.csv
-# <br></br>
-# The Hospitality employment level data was obtained from the Bureau of Labor Statistics, local and county data.  As my location is in
-# southwest Colorado, the Grand Junction data were more applicable.</p>
-# <br></br>
-# <h3>Processing</h3><br></br>
-# <p>The main plot indicates the Cases per County, across dates.  In the plot, the yellow line is a plot of the
-# Hospitality employment.  In this case, only the months capturing the aggressive increases in <q>opening up</q>.
-# <br></br>
-# Conceptually, we would expect as things open up, the delay in the increase in Cases is unknown.  In other words, while
-# it is known that the virus can take 10 to 14 days to take hold, this is affected by the level of interaction.  This level of interaction
-# is proxied by the opening of restaurants, bars, resorts, etc.
-# <br></br>
-# A simple GLM is fit (Cases ~ Hospitality), but with no delay (0 weeks) it is observing an increase in Hospitality
-# with no immediate increase in Cases (as it takes time for COVID-19 to spread).  The smaller plot is a histogram of the
-# model residuals (SSE), with the lowest/best fit signaled in green.
-# <br></br>
-# As you investigate/increase the delay, the model has a better fit in the future, and at some point drops off in accuracy.
-#  Therefore this application can give one a feel for the length of time it takes for an observed/new increase in employment to
-# manifest as an increase in Cases.</p>"))),
-tabPanel("Github", fluidPage(HTML("
-<h2>Links to Github code...</h2>
-<p><a href='https://github.com/mountainray/covid_delay_app/blob/main/ui.R'>ui.R</a></p>
+	    #helpText("Sum of Squared Errors, Cases ~ Hospitality"),
+            tableOutput("tablex")))),
 
+tabPanel("Github Code", fluidPage(HTML("
+<h2>Code generating this application can be found on Github...</h2>
+<p><a href='https://github.com/mountainray/covid_delay_app/blob/main/ui.R'>ui.R</a></p>
 <p><a href='https://github.com/mountainray/covid_delay_app/blob/main/server.R'>server.R</a><p>")))
 ))
 
