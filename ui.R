064a6f18d823db17cdb4a6941cdd91bc2ac74970
mountainray <mountainray@hotmail.com>
2020-11-21 15:19:36 -0700

    working version 2, updated Documentation, links formatting

--- ui.R
+++ ui.R
@@ -49,6 +49,33 @@ county_names$geom[1]
 shinyUI(navbarPage("COVID-19 Delay Application", theme = "bootstrap.css",
 #shinyUI(fluidPage(
 #    titlePanel(HTML("Colorado County-level COVID data<br></br><h4>Detecting delayed effects of changes in Hospitality Employment levels</h4>")),
+tabPanel("Documentation", fluidPage(HTML("
+<h2>Documentation...</h3>
+<h3>Concept</h3>
+<p>With the pandemic raging, it was of interest to see how well one could assess when the ramifications of <q>opening up</q> would
+be observed as increased case counts.  Obviously monthly employment level data (in this case for Hospitality) over 2020 has seen some
+appreciable increases as bars, restaurants, clubs and resorts, etc. were opened more and more.</p>
+<p>With the situation (at least) somewhat politicized, and with the economic effects acute, this tool may serve as a basis for a more
+robust approach.  This would allow one to estimate the degree to which a county or area (multiple counties) perpetuates COVID-19 spread
+given public health administration guidelines.</p>
+
+<h3>Data</h3>
+<p>The Colorado Case data on COVID-19 positivity is updated daily, and can be found
+<a href='https://opendata.arcgis.com/datasets/1456d8d43486449292e5784dcd9ce4a7_0.csv'>here</a>.</p>
+<p>The Hospitality employment level data was obtained from the Bureau of Labor Statistics, local and county data.  As my location is in
+southwest Colorado, the <a href='https://www.bls.gov/regions/mountain-plains/co_grandjunction_msa.htm#eag'>Grand Junction</a> data were more applicable.</p>
+<h3>Processing</h3>
+<p>The main plot indicates the Cases per County, across dates.  In the plot, the yellow line is a plot of the
+Hospitality employment.  In this case, only the months capturing the aggressive increases in <q>opening up</q>.</p>
+<p>Conceptually, we would expect as things open up, the delay in the increase in Cases is unknown.  In other words, while
+it is known that the virus can take 10 to 14 days to take hold, this is affected by the level of interaction.  This level of interaction
+is proxied by the opening of restaurants, bars, resorts, etc.</p>
+<p>A simple GLM is fit (<mark>Cases ~ Hospitality</mark>), but with no delay (0 weeks) it is observing an increase in Hospitality
+with no immediate increase in Cases (as it takes time for COVID-19 to spread).  The smaller plot is a histogram of the
+model residuals (SSE), with the lowest/best fit signaled in green.</p>
+<p>As you investigate/increase the delay, the model has a better fit in the future, and at some point drops off in accuracy.
+ Therefore this application can give one a feel for the length of time it takes for an observed/new increase in employment to
+manifest as an increase in Cases.</p>"))),
 tabPanel("Main",
     titlePanel(HTML("Colorado County-level COVID data\n<h4>Detecting delayed effects of changes in Hospitality Employment levels</h4><br></br>")),
     # fluidRow("a try"),
@@ -81,33 +108,34 @@ helpText("The slider above determines the delay (in weeks) from observed changes
         )
     )
 ),
-tabPanel("Documentation", fluidPage(HTML("
-<h2>Documentation...</h3><br></br>
-<h3>Data</h3><br></br>
-<p>The Colorado Case data on COVID-19 positivity is updated daily, and can be found here:<br></br>
-https://opendata.arcgis.com/datasets/1456d8d43486449292e5784dcd9ce4a7_0.csv<br></br>
+# tabPanel("Documentation", fluidPage(HTML("
+# <h2>Documentation...</h3><br></br>
+# <h3>Data</h3><br></br>
+# <p>The Colorado Case data on COVID-19 positivity is updated daily, and can be found here:<br></br>
+# https://opendata.arcgis.com/datasets/1456d8d43486449292e5784dcd9ce4a7_0.csv
+# <br></br>
+# The Hospitality employment level data was obtained from the Bureau of Labor Statistics, local and county data.  As my location is in
+# southwest Colorado, the Grand Junction data were more applicable.</p>
+# <br></br>
+# <h3>Processing</h3><br></br>
+# <p>The main plot indicates the Cases per County, across dates.  In the plot, the yellow line is a plot of the
+# Hospitality employment.  In this case, only the months capturing the aggressive increases in <q>opening up</q>.
+# <br></br>
+# Conceptually, we would expect as things open up, the delay in the increase in Cases is unknown.  In other words, while
+# it is known that the virus can take 10 to 14 days to take hold, this is affected by the level of interaction.  This level of interaction
+# is proxied by the opening of restaurants, bars, resorts, etc.
+# <br></br>
+# A simple GLM is fit (Cases ~ Hospitality), but with no delay (0 weeks) it is observing an increase in Hospitality
+# with no immediate increase in Cases (as it takes time for COVID-19 to spread).  The smaller plot is a histogram of the
+# model residuals (SSE), with the lowest/best fit signaled in green.
+# <br></br>
+# As you investigate/increase the delay, the model has a better fit in the future, and at some point drops off in accuracy.
+#  Therefore this application can give one a feel for the length of time it takes for an observed/new increase in employment to
+# manifest as an increase in Cases.</p>"))),
+tabPanel("Github", fluidPage(HTML("
+<h2>Links to Github code...</h2>
+<p><a href='https://github.com/mountainray/covid_delay_app/blob/main/ui.R'>ui.R</a></p>
 
-The Hospitality employment level data was obtained from the Bureau of Labor Statistics, local and county data.  As my location is in
-southwest Colorado, the Grand Junction data were more applicable.</p><br></br>
-
-<h3>Processing</h3><br></br>
-<p>The main plot indicates the Cases per County, across dates.  In the plot, the yellow line is a plot of the
-Hospitality employment.  In this case, only the months capturing the aggressive increases in <q>opening up</q>.
-<br></br>
-Conceptually, we would expect as things open up, the delay in the increase in Cases is unknown.  In other words, while
-it is known that the virus can take 10 to 14 days to take hold, this is affected by the level of interaction.  This level of interaction
-is proxied by the opening of restaurants, bars, resorts, etc.<br></br>
-A simple GLM is fit (Cases ~ Hospitality), but with no delay (0 weeks) it is observing an increase in Hospitality
-with no immediate increase in Cases (as it takes time for COVID-19 to spread).  The smaller plot is a histogram of the
-model residuals (SSE), with the lowest/best fit signaled in green.
-<br></br>
-As you investigate/increase the delay, the model has a better fit in the future, and at some point drops off in accuracy.
-Therefore this application can give one a feel for the length of time it takes for an observed/new increase in employment to
-manifest as an increase in Cases.</p>
-<br></br>
-<h3>Links to Github code...</h3><br></br>
-<a href='https://github.mountainray.com/covid_explorer/ui.R'>ui.R</a>
-<br></br>
-<a href='https://github.mountainray.com/covid_explorer/server.R'>server.R</a>
-				   ")))))
+<p><a href='https://github.com/mountainray/covid_delay_app/blob/main/server.R'>server.R</a><p>")))
+))
 
